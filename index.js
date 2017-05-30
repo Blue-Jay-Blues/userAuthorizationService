d6d18ddcd1394284bcda635ef6659ea6e0f2a0cc
David Moneysmith <moneysmith777@gmail.com>
2017-05-30 10:03:45 -0400

    Removed and cleaned up old code

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const session = require('express-session')
 const app = express();
-// const mongoose = require('mongoose');
 const passport = require('passport');
 
-//Setup Database:
-// const database = require('./database');
-// mongoose.connect('mongodb://localhost/test');
-// mongoose.connect(database.url);
-
 //Setup Passport:
-app.use(session({secret: '___'}));  //I'm not sure what this does.
+app.use(session({secret: 'mySecret'}));  //Note: Replace with actual secret in production
 app.use(passport.initialize());
 app.use(passport.session());  //Make sessions persistent
 
@@ -19,3 +13,4 @@ require('./route')(app, passport);
 
 //Start Server:
 app.listen(8080, () => console.log('User Authentication Service listening on port 8080'));
+
